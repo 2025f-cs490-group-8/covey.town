de6e44188a59a0da41181e240a667c6a8961b275
Sahil Parmar <108904279+SahilP20@users.noreply.github.com>
2025-11-02 12:05:02 -0500

    Update GameArea.ts

--- townService/src/town/games/GameArea.ts
+++ townService/src/town/games/GameArea.ts
@@ -1,34 +1,64 @@
-import Interactable, { KnownInteractableTypes } from '../Interactable';
+import InvalidParametersError, {
+  PLAYER_NOT_IN_GAME_MESSAGE,
+} from '../../lib/InvalidParametersError';
+import Player from '../../lib/Player';
+import {
+  GameArea as GameAreaModel,
+  GameResult,
+  GameState,
+  InteractableType,
+} from '../../types/CoveyTownSocket';
+import InteractableArea from '../InteractableArea';
+import Game from './Game';
 
-export default class GameArea extends Interactable {
-  private _isInteracting = false;
+/**
+ * A GameArea is an InteractableArea on the map that can host a game.
+ * At any given point in time, there is at most one game in progress in a GameArea.
+ */
+export default abstract class GameArea<
+  GameType extends Game<GameState, unknown>,
+> extends InteractableArea {
+  protected _game?: GameType;
 
-  addedToScene() {
-    super.addedToScene();
-    this.setTintFill();
-    this.setAlpha(0.3);
-    this.setDepth(-1);
-    this.scene.add.text(
-      this.x - this.displayWidth / 2,
-      this.y + this.displayHeight / 2,
-      this.name,
-      { color: '#FFFFFF', backgroundColor: '#000000' },
-    );
+  protected _history: GameResult[] = [];
+
+  public get game(): GameType | undefined {
+    return this._game;
   }
 
-  overlapExit(): void {
-    if (this._isInteracting) {
-      this.townController.interactableEmitter.emit('endInteraction', this);
-      this._isInteracting = false;
+  public get history(): GameResult[] {
+    return this._history;
+  }
+
+  public toModel(): GameAreaModel<GameType['state']> {
+    return {
+      id: this.id,
+      game: this._game?.toModel(),
+      history: this._history,
+      occupants: this.occupantsByID,
+      type: this.getType(),
+    };
+  }
+
+  public get isActive(): boolean {
+    return true;
+  }
+
+  protected abstract getType(): InteractableType;
+
+  public remove(player: Player): void {
+    if (this._game) {
+      try {
+        this._game.leave(player);
+      } catch (e) {
+        if ((e as InvalidParametersError).message === PLAYER_NOT_IN_GAME_MESSAGE) {
+          // do nothing
+        } else {
+          throw e;
+        }
+      }
     }
-  }
-
-  interact(): void {
-    this._isInteracting = true;
-  }
-
-  getType(): KnownInteractableTypes {
-    return 'gameArea';
+    super.remove(player);
   }
 }
 
