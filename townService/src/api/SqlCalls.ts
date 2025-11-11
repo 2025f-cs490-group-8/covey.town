5c5da4c48b3143ea71246825b9a613aa6d748b0c
lbiomye <ryan.lobur@gmail.com>
2025-11-11 18:09:11 -0500

    added the remain function headers and fixed ending the connection

--- townService/src/api/SqlCalls.ts
+++ townService/src/api/SqlCalls.ts
@@ -1,6 +1,6 @@
 
 import { createPool } from 'mysql2/promise'; 
- import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcrypt';
 
 function constructBCRYPTHash(password: string) {
     const salt = 10;
@@ -29,27 +29,93 @@ export class UserRepository {
             const row = await connection.execute(query);
             return row;
         } catch (error) {
-            console.error('Error fetching users:', error);
+            console.error('Error fetching user:', error);
             throw error;
+        } finally {
+            await connection.end();
         }
     }
 
-    /**
-     * id is taken care of through mysql and status has on offline default value.
-     * 
-     */
-    async constructNewUser(userName: string, email: string, userPassword: string) {
+    protected async getUserHash(uid: number) {
         try {
-            const hash = constructMD5Hash(userPassword)
-            const query = 'INSERT INTO Users (userName, email, hash) VALUES (' + userName + ', ' + email + ', ' + hash + ')'; 
+            const query = 'SELECT hash FROM Users WHERE id = ' + uid; 
             const row = await connection.execute(query);
             return row;
         } catch (error) {
-            console.error('Error constructing user:', error);
+            console.error('Error fetching hash:', error);
             throw error;
+        } finally {
+            await connection.end();
         }
     }
 
+    async setUserStatus(uid: number, status: string) {
+        try {
+            const query = 'UPDATE Users SET status = ' + status + ' WHERE id = ' + uid; 
+            await connection.execute(query);
+        } catch (error) {
+            console.error('Error updating user status:', error);
+            throw error;
+        } finally {
+            await connection.end();
+        }
+    }
+    /**
+     * id is taken care of through mysql and status has on offline default value.
+     */
+    async constructNewUser(userName: string, email: string, userPassword: string) {
+        try {
+            const hash = constructBCRYPTHash(userPassword)
+            const query = 'INSERT INTO Users (userName, email, hash) VALUES (' + userName + ', ' + email + ', ' + hash + ')'; 
+            await connection.execute(query);
+        } catch (error) {
+            console.error('Error constructing user:', error);
+            throw error;
+        } finally {
+            await connection.end();
+        }
+    }
+    /**
+     * default pending status
+     */
+    async constructNewFriendRequest(sender: string, receiver: string) {
+        try {
+            const query = 'INSERT INTO FriendRequests (sender, receiver) VALUES (' + sender + ', ' + receiver + ')'; 
+            await connection.execute(query);
+        } catch (error) {
+            console.error('Error constructing friend request:', error);
+            throw error;
+        } finally {
+            await connection.end();
+        }
+    }
+    /**
+     * note for ryan: need to get block sender and reciever and run list operations on it should return status's as well for ease.
+     */
+    async getFriendsList(){
+        return;
+    }
 
-
+    async acceptFriendRequest(){
+        return;
+    }
+    /**
+     * this will directly delete the request from our database, can be used to unadd someone, if a user wishes to clock a friend call
+     * blockUser() instead.
+     */
+    async declineFriendRequest(){
+        return;
+    }
+    /**
+     * if a user chooses to block someone during a friendrequest prompt only call blockuser it declines and deletes the request for you
+     */
+    async blockUser(){
+        return;
+    }
+    /**
+     * deletes blocked relationship from table
+     */
+    async unblockUser(){
+        return;
+    }
 }
