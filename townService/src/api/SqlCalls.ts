9e78ad8c51e33368799483068415534475eb56cf
lbiomye <ryan.lobur@gmail.com>
2025-11-11 17:24:03 -0500

    added hash function and getUser / constructUser functions

--- townService/src/api/SqlCalls.ts
+++ townService/src/api/SqlCalls.ts
@@ -1,27 +1,42 @@
-import sql from 'mssql' 
-import dotenv from 'dotenv';
 
-dotenv.config();
+import { createPool } from 'mysql2/promise'; 
+import * as CryptoJS from 'crypto-js';
 
-const config = {
-    user: 'admin',
-    password: 'epicgamer12',
-    server: 'covey-town.c1e4guig85zc.us-east-2.rds.amazonaws.com',
-    database: 'COVEYTOWN',
-    options: {
-        encrypt: true,
-        trustServerCertificate: true
-    }
-};
-
-async function connectToDB() {
-    try {
-        await sql.connect(config);
-        console.log('SQL Connection Successful');
-    } catch (err) {
-        console.error('SQl Connection Failed:', err);
-    }
-    
+function constructMD5Hash(password: string) {
+    return CryptoJS.MD5(password).toString();
 }
 
-connectToDB()
+export const connection = createPool({
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT || '3306'),
+    user: process.env.DB_USER || 'admin',
+    password: process.env.DB_PASSWORD || 'epicgamer12', 
+    database: process.env.DB_NAME || 'COVEYTOWN',
+});
+
+
+export class UserRepository {
+    async getUser(uid: number) {
+        try {
+            const query = 'SELECT userName, email, status FROM Users WHERE id = ' + uid; 
+            const row = await connection.execute(query);
+            return row;
+        } catch (error) {
+            console.error('Error fetching users:', error);
+            throw error;
+        }
+    }
+
+    async constructNewUser(userName: string, email: string, userPassword: string) {
+        try {
+            const hash = constructMD5Hash(userPassword)
+            const query = 'INSERT INTO Users (userName, email, hash) VALUES (' + userName + ', ' + email + ', ' + hash + ')'; 
+            const row = await connection.execute(query);
+            return row;
+        } catch (error) {
+            console.error('Error fetching users:', error);
+            throw error;
+        }
+    }
+          
+}
