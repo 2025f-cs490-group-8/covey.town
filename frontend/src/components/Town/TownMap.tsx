ec2f7a3e55af84aab038679c79ca653de4317b39
dathomir01 <codfreak91@gmail.com>
2025-12-11 00:15:09 -0500

    Removes function-less button

--- frontend/src/components/Town/TownMap.tsx
+++ frontend/src/components/Town/TownMap.tsx
@@ -10,7 +10,7 @@ import useChatContext from '../VideoCall/VideoFrontend/hooks/useChatContext/useC
 import ChatWindow from '../VideoCall/VideoFrontend/components/ChatWindow/ChatWindow';
 import clsx from 'clsx';
 import { createStyles, makeStyles, Theme } from '@material-ui/core';
-import ProfileButton from '../SocialSidebar/ProfileButton';
+import Profile from '../SocialSidebar/Profile';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -93,7 +93,7 @@ export default function TownMap(): JSX.Element {
       <div id='map-container' />
       <div id='social-container'>
         <SocialSidebar />
-        <ProfileButton />
+        <Profile />
       </div>
     </div>
   );
